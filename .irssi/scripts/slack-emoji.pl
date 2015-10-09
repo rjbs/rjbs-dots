596038810556c7feb83c51fabd468c6395e91ab4
Ricardo Signes <rjbs@cpan.org>
2015-10-08 22:37:46 -0400

    slack emoji script

---
+++ .irssi/scripts/slack-emoji.pl
@@ -0,0 +1,44 @@
+use warnings;
+use strict;
+
+use charnames ':full';
+use Irssi ();
+
+our $VERSION = '0.001';
+our %IRSSI = (
+  authors => 'rjbs',
+  name    => 'slack-emoji',
+);
+
+my %emoji = (
+  'imp'   => "\N{IMP}",
+  'heart' => "\N{BLUE HEART}", # Pobox, yo.
+  'poop'  => "\N{PILE OF POO}",
+  'smile' => "\N{SMILING FACE WITH OPEN MOUTH AND SMILING EYES}",
+  '+1'    => "\N{THUMBS UP SIGN}",
+  '-1'    => "\N{THUMBS DOWN SIGN}",
+);
+
+sub munge_emoji {
+  my ($target, $text) = split / :/, $_[0], 2;
+  # make this :[^:]+: later --rjbs
+  $text =~ s/:$_:/$emoji{$_}/g for keys %emoji;
+  return "$target :$text";
+}
+
+sub expand_emoji {
+  my ($server, $data, $nick, $address) = @_;
+  return unless $server->{chatnet} =~ /slack/;
+
+  Irssi::signal_stop();
+  Irssi::signal_remove('event privmsg', 'expand_emoji');
+  Irssi::signal_emit('event privmsg',
+    $server,
+    munge_emoji($data),
+    $nick,
+    $address,
+  );
+  Irssi::signal_add('event privmsg', 'expand_emoji');
+}
+
+Irssi::signal_add('event privmsg'  => 'expand_emoji');
