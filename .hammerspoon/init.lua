1b0df137283c2873c2f4003493b55c340f67e151
Ricardo Signes <rjbs@cpan.org>
2019-07-05 10:14:34 -0400

    resize more things

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,12 +1,15 @@
+
+
 function resizeChromes ()
   local chrome = hs.application.find("Chrome")
   if (chrome) then
     local windows = chrome:visibleWindows()
     for i, window in ipairs(windows) do
+      local frame = window:screen():frame()
       if (string.len(window:title()) > 0) then
         window:move({
-          x = 10 * (i - 1),
-          y = 10 * (i - 1),
+          x = frame.x + 10 * (i - 1),
+          y = frame.y + 10 * (i - 1),
           w = 1200,
           h = 800
         })
@@ -15,6 +18,26 @@ function resizeChromes ()
   end
 end
 
+function resizeSlack ()
+  local slack = hs.application.find("Slack")
+  if (slack) then
+    local windows = slack:visibleWindows()
+    for i, window in ipairs(windows) do
+      local frame = window:screen():fullFrame()
+      window:move({
+        x = frame.w - 960,
+        y = frame.h - 710,
+        w = 950,
+        h = 700
+      })
+      break -- one window is enough, in case of madness
+    end
+  end
+end
+
 resizeMenu = hs.menubar.new()
 resizeMenu:setTitle("⌧")
-resizeMenu:setClickCallback(resizeChromes)
+resizeMenu:setClickCallback(function ()
+  resizeChromes()
+  resizeSlack()
+end)
