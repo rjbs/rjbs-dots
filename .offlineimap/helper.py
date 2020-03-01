00cdf22260a055d76b5f4399ed74c64145d45d1e
Ricardo Signes <rjbs@semiotic.systems>
2020-03-01 07:24:55 -0500

    offlineimap: stupid ordering

--- .offlineimap/helper.py
+++ .offlineimap/helper.py
@@ -6,3 +6,18 @@ def fm_nametrans_remote(folder):
 def fm_nametrans_local(folder):
   if folder == 'INBOX': return 'INBOX'
   return 'INBOX.' + folder
+
+prioritized = [ 'Sent', 'Archive.2019.11' ]
+
+def mycmp(x, y):
+  for prefix in prioritized:
+    xsw = x.startswith(prefix)
+    ysw = y.startswith(prefix)
+    if xsw and ysw:
+      return cmp(x, y)
+    elif xsw:
+      return -1
+    elif ysw:
+      return +1
+  return cmp(x, y)
+
