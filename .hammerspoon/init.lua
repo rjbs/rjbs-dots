5b56812299d5173a16e3d4bc1a1f918050cdc1e1
Ricardo Signes <rjbs@semiotic.systems>
2020-08-08 21:16:28 -0400

    hammerspoon: each desktop gets its own staggering

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,12 +3,29 @@ function resizeChromes ()
   local chrome = hs.application.find("Chrome")
   if (chrome) then
     local windows = chrome:visibleWindows()
+
+    -- So, we're going to move windows to near the top left of their screens,
+    -- staggering them by +(10,10) per window.  But the staggering needs to be
+    -- on a per-desktop basis or it's weird.  (You end up with gaps in the
+    -- stagging of desktop A while windows on desktop B are positioned to fill
+    -- those gaps in relative positioning.  Weird, right?)  I don't think
+    -- there's a simple desktop id, so instead I'm going to use the string
+    -- "x,y" of the frame of the window's screen as the key for storing the
+    -- offset coefficient incrementor. -- rjbs, 2020-08-08
+    local offset = {}
+
     for i, window in ipairs(windows) do
       local frame = window:screen():frame()
       if (string.len(window:title()) > 0) then
+        -- Woah, what's up with that string.len() call?  Well, some UI elements
+        -- like "Find" boxes are actually windows.  Trying to move them or
+        -- stagger based on them would be a fool's errand. -- rjbs, 2020-08-08
+        local key = frame.x .. "," .. frame.y
+        offset[key] = (offset[key] or 0) + 1
+
         window:move({
-          x = frame.x + 10 * i,
-          y = frame.y + 10 * i,
+          x = frame.x + 10 * offset[key],
+          y = frame.y + 10 * offset[key],
           w = 1200,
           h = 800
         })
